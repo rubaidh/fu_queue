f3e235aec643243b2e9eade3d2e2eefa15faf968
Graeme Mathieson <mathie@woss.name>
2009-06-18 12:34:31 +0100

    Skeleton plugin.

---
+++ tasks/fu_queue_tasks.rake
@@ -0,0 +1,4 @@
+# desc "Explaining what the task does"
+# task :fu_queue do
+#   # Task goes here
+# end
---
+++ test/fu_queue_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class FuQueueTest < ActiveSupport::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
